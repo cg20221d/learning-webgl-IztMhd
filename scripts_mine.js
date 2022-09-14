d56782149c26caba8d72b00ec1c10cbaf981be13
izzati.m1314 <izzati.m1314@gmail.com>
2022-09-14 18:34:24 +0700

    bikin diamond, file mine program blm bekerja, aneh

--- scripts_mine.js
+++ scripts_mine.js
@@ -60,3 +60,66 @@ function main() {
 
     gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
 }
+
+function main() {
+    var kanvas = document.getElementById("kanvas");
+    var gl = kanvas.getContext("webgl");
+
+    var vertices = [
+        1.0, 1.0,       //A: kanan atas
+        0.0, 0.0,       //B: bawah tengah
+        -1.0, 1.0,
+                //C: kiri atas
+    ];
+
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+    // Vertex shader
+    var vertexShaderCode = `
+    attribute vec2 aPosition;
+    void main() {
+        float x = aPosition.x;
+        float y = aPosition.y;
+        gl_PointSize = 10.0;
+        gl_position = vec4(x, y, 0.0, 1.0);
+    }
+    `;
+    var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShaderObject, vertexShaderCode);
+    gl.compileShader(vertexShaderObject);   // sampai sini sudah jadi .o
+
+    // Fragment shader
+    var fragmentShaderCode = `
+    precision mediump float;
+    void main() {
+        float r = 0.0;
+        float g = 0.0;
+        float b = 1.0;
+        gl_fragColor = vec4(0.0, 0.0 , 1.0, 1.0); 
+    }
+    `;
+    var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
+    gl.compileShader(fragmentShaderObject);   // sampai sini sudah jadi .o
+
+    var shaderProgram = gl.createProgram(); // wadah dari executable (.exe)
+    gl.attachShader(shaderProgram, vertexShaderObject);
+    gl.attachShader(shaderProgram, fragmentShaderObject);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+
+    //Mengajari GPU caranya mengoleksi nilai posisi ARRAY_BUFFER
+    //Untuk Setiapverteks yang di[roses
+    //
+    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+
+    gl.clearColor(1.0,      0.65,    0.0,    1.0);
+    //            Merah     Hijau   Biru    Transparansi
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.LINE_LOOP, 0, 3);
+}
